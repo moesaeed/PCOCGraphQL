454873788a06a147823748ec98be867090e44aaa
MohammadSaeed <shank187@gmail.com>
2024-05-30 12:21:05 +0300

    add DynamicModulesNames

--- DF2023/Core/Constants/DynamicModulesNames.cs
+++ DF2023/Core/Constants/DynamicModulesNames.cs
@@ -5,4 +5,308 @@
         public const string NotificationType = "Telerik.Sitefinity.DynamicTypes.Model.Conferences.Notification";
         public const string FCMListType = "Telerik.Sitefinity.DynamicTypes.Model.Conferences.Fcmlist";
     }
+
+    public static class Airport
+    {
+        public const string AirportDynamicTypeName = "Telerik.Sitefinity.DynamicTypes.Model.Airports.Airport";
+        public const string TitleAr = "TitleAr";
+        public const string Title = "Title";
+    }
+
+    public static class Badgetype
+    {
+        public const string BadgetypeDynamicTypeName = "Telerik.Sitefinity.DynamicTypes.Model.BadgeTypes.Badgetype";
+        public const string Title = "Title";
+        public const string TitleAr = "TitleAr";
+    }
+
+    public static class Convention
+    {
+        public const string ConventionDynamicTypeName = "Telerik.Sitefinity.DynamicTypes.Model.Conventions.Convention";
+
+        public const string StartDate = "StartDate";
+
+        public const string EndDate = "EndDate";
+
+        public const string Notes = "Notes";
+
+        public const string InvitationEmail = "InvitationEmail";
+
+        public const string Title = "Title";
+
+        public const string UseCustomInvitation = "UseCustomInvitation";
+
+        public const string Logo = "Logo";
+
+        public const string JSONFormSettings = "JSONFormSettings";
+
+        public const string Venue = "Venue";
+
+        public const string TitleAr = "TitleAr";
+
+        public const string InvitationEmailSubject = "InvitationEmailSubject";
+
+        public const string ShortUniqueName = "ShortUniqueName";
+    }
+
+    public static class Country
+    {
+        public const string CountryDynamicTypeName = "Telerik.Sitefinity.DynamicTypes.Model.Countries.Country";
+
+        public const string Nationality = "Nationality";
+
+        public const string Alpha2 = "Alpha2";
+
+        public const string Title = "Title";
+
+        public const string IsArabic = "IsArabic";
+
+        public const string Flag = "Flag";
+
+        public const string Alpha3 = "Alpha3";
+
+        public const string NationalityAr = "NationalityAr";
+
+        public const string TitleAr = "TitleAr";
+    }
+
+    public static class CustomTextConfig
+    {
+        public const string CustomTextConfigDynamicTypeName = "Telerik.Sitefinity.DynamicTypes.Model.Conventions.CustomTextConfig";
+
+        public const string InvitationEmailSubject = "InvitationEmailSubject";
+
+        public const string ServiceLevel = "ServiceLevel";
+
+        public const string LandingPage = "LandingPage";
+
+        public const string ConfirmationEmail = "ConfirmationEmail";
+
+        public const string InvitationEmail = "InvitationEmail";
+
+        public const string ConfirmationPage = "ConfirmationPage";
+
+        public const string ConfirmationEmailSubject = "ConfirmationEmailSubject";
+
+        public const string Title = "Title";
+    }
+
+    public static class Delegation
+    {
+        public const string DelegationDynamicTypeName = "Telerik.Sitefinity.DynamicTypes.Model.Conventions.Delegation";
+
+        public const string ContactPhoneNumber = "ContactPhoneNumber";
+
+        public const string ContactEmail = "ContactEmail";
+
+        public const string Title = "Title";
+
+        public const string NumberOfOfficialDelegates = "NumberOfOfficialDelegates";
+
+        public const string Entity = "Entity";
+
+        public const string Guests = "Guests";
+
+        public const string TitleAr = "TitleAr";
+
+        public const string RemainingNumberOfOfficialDelegates = "RemainingNumberOfOfficialDelegates";
+
+        public const string IsSingle = "IsSingle";
+
+        public const string ServicesLevel = "ServicesLevel";
+
+        public const string InvitationDate = "InvitationDate";
+
+        public const string Country = "Country";
+
+        public const string DelegationJSON = "DelegationJSON";
+
+        public const string ContactName = "ContactName";
+
+        public const string SecondaryEmail = "SecondaryEmail";
+    }
+
+    public static class Delegationmembertype
+    {
+        public const string DelegationmembertypeDynamicTypeName = "Telerik.Sitefinity.DynamicTypes.Model.DelegationMemberTypes.Delegationmembertype";
+
+        public const string Title = "Title";
+
+        public const string TitleAr = "TitleAr";
+    }
+
+    public static class DelegationType
+    {
+        public const string DelegationTypeDynamicTypeName = "Telerik.Sitefinity.DynamicTypes.Model.DelegationTypes.DelegationType";
+
+        public const string TitleAr = "TitleAr";
+
+        public const string Title = "Title";
+    }
+
+    public static class Entity
+    {
+        public const string EntityDynamicTypeName = "Telerik.Sitefinity.DynamicTypes.Model.Entities.Entity";
+
+        public const string Title = "Title";
+    }
+
+    public static class Guest
+    {
+        public const string GuestDynamicTypeName = "Telerik.Sitefinity.DynamicTypes.Model.Conventions.Guest";
+
+        public const string LastName = "LastName";
+
+        public const string InvitationDate = "InvitationDate";
+
+        public const string SecondaryEmail = "SecondaryEmail";
+
+        public const string JobTitle = "JobTitle";
+
+        public const string Email = "Email";
+
+        public const string SecondName = "SecondName";
+
+        public const string SpecialLogisticsRequirements = "SpecialLogisticsRequirements";
+
+        public const string PersonalPhoto = "PersonalPhoto";
+
+        public const string Notes = "Notes";
+
+        public const string GuestJSON = "GuestJSON";
+
+        public const string ServicesLevel = "ServicesLevel";
+
+        public const string DepartureAirport = "DepartureAirport";
+
+        public const string SubAttendeeType = "SubAttendeeType";
+
+        public const string FullNameAr = "FullNameAr";
+
+        public const string OrganizationName = "OrganizationName";
+
+        public const string GuestStatus = "GuestStatus";
+
+        public const string FourthNameAr = "FourthNameAr";
+
+        public const string RegistrationDate = "RegistrationDate";
+
+        public const string FirstName = "FirstName";
+
+        public const string SourceOfInvitation = "SourceOfInvitation";
+
+        public const string PassportPhoto = "PassportPhoto";
+
+        public const string PassportExpiryDate = "PassportExpiryDate";
+
+        public const string PassportType = "PassportType";
+
+        public const string SubscribeToNewsletter = "SubscribeToNewsletter";
+
+        public const string OpeningCeremony = "OpeningCeremony";
+
+        public const string Title = "Title";
+
+        public const string BadgeType = "BadgeType";
+
+        public const string PhoneAreaCode = "PhoneAreaCode";
+
+        public const string IsLocal = "IsLocal";
+
+        public const string RejectNote = "RejectNote";
+
+        public const string PassportCountry = "PassportCountry";
+
+        public const string SpeakerPhotograph = "SpeakerPhotograph";
+
+        public const string SecondNameAr = "SecondNameAr";
+
+        public const string FourthName = "FourthName";
+
+        public const string EquipmentOrBiography = "EquipmentOrBiography";
+
+        public const string GalaDinner = "GalaDinner";
+
+        public const string Nationality = "Nationality";
+
+        public const string FirstNameAr = "FirstNameAr";
+
+        public const string LastNameAr = "LastNameAr";
+
+        public const string Gender = "Gender";
+
+        public const string BatchLogisticsStatus = "BatchLogisticsStatus";
+
+        public const string ThirdNameAr = "ThirdNameAr";
+
+        public const string DOB = "DOB";
+
+        public const string ThirdName = "ThirdName";
+
+        public const string PersonTitle = "PersonTitle";
+
+        public const string DelegationMemberType = "DelegationMemberType";
+
+        public const string ResidenceCountry = "ResidenceCountry";
+
+        public const string GuestStageStatus = "GuestStageStatus";
+
+        public const string PassportNumber = "PassportNumber";
+
+        public const string Phone = "Phone";
+    }
+
+    public static class Guestposition
+    {
+        public const string GuestpositionDynamicTypeName = "Telerik.Sitefinity.DynamicTypes.Model.GuestPositions.Guestposition";
+
+        public const string TitleAr = "TitleAr";
+
+        public const string Title = "Title";
+    }
+
+    public static class Gueststatus
+    {
+        public const string GueststatusDynamicTypeName = "Telerik.Sitefinity.DynamicTypes.Model.GuestStatus.Gueststatus";
+
+        public const string TitleAr = "TitleAr";
+
+        public const string Title = "Title";
+    }
+
+    public static class Passporttype
+    {
+        public const string PassporttypeDynamicTypeName = "Telerik.Sitefinity.DynamicTypes.Model.PassportTypes.Passporttype";
+
+        public const string TitleAr = "TitleAr";
+
+        public const string Title = "Title";
+    }
+
+    public static class ServicesLevel
+    {
+        public const string ServicesLevelDynamicTypeName = "Telerik.Sitefinity.DynamicTypes.Model.ServiceLevels.ServicesLevel";
+
+        public const string TitleAr = "TitleAr";
+
+        public const string Title = "Title";
+    }
+
+    public static class Subattendeetype
+    {
+        public const string SubattendeetypeDynamicTypeName = "Telerik.Sitefinity.DynamicTypes.Model.SubAttendeeTypes.Subattendeetype";
+
+        public const string TitleAr = "TitleAr";
+
+        public const string Title = "Title";
+    }
+
+    public static class Titlelist
+    {
+        public const string TitlelistDynamicTypeName = "Telerik.Sitefinity.DynamicTypes.Model.TitleList.Titlelist";
+
+        public const string TitleAr = "TitleAr";
+
+        public const string Title = "Title";
+    }
 }
