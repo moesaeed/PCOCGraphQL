215fccc2eb11257f5297d9d93cad0035f11520e2
MohammadSaeed <shank187@gmail.com>
2024-05-31 20:34:10 +0300

    Lockout user in case of 5 wrong passworda

---
+++ DF2023/CustomMembershipProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using Telerik.Sitefinity.Security.Data;
+using Telerik.Sitefinity.Security.Model;
+
+namespace DF2023
+{
+    public class CustomMembershipProvider : OpenAccessMembershipProvider
+    {
+        protected override void UpdateFailureCount(User user, string failureType)
+        {
+            base.UpdateFailureCount(user, failureType);
+            if (HttpContext.Current.Request.Url.LocalPath.Contains("sitefinity/oauth/token") && failureType == "password") //only run this logic when requests to this endpoint are made and the failure type is because of incorrect password.
+            {
+                var provider = ((Telerik.Sitefinity.Model.IDataItem)user).Provider as OpenAccessMembershipProvider;
+                provider.SuppressSecurityChecks = true; //under the current context, there is no user being authenticated. We need to suppress the checks so that we can commit changes to the DB.
+
+                provider.CommitTransaction();
+
+                provider.SuppressSecurityChecks = false; //reestablishing the security checks on provider level after the change is committed.
+            }
+        }
+    }
+}
